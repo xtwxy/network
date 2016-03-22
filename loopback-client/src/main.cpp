1e1570757ea051ed00b9123009ec7272035d20b6
xtwxy <xtwxy@hotmail.com>
2016-03-22 21:23:57 +0800

    complete the loopback client.

--- loopback-client/src/main.cpp
+++ loopback-client/src/main.cpp
@@ -7,30 +7,70 @@
 
 const static int BUFFER_SIZE = 4096;
 
-char buffer[BUFFER_SIZE];
+char readBuffer[BUFFER_SIZE];
+char writeBuffer[BUFFER_SIZE];
+
+boost::asio::io_service ios;
+boost::asio::ip::tcp::resolver resolver(ios);
+boost::asio::ip::tcp::socket sock(ios);
+
+void onReadComplete(const boost::system::error_code& ec,
+		size_t bytes_transferred);
+void onWriteComplete(const boost::system::error_code& ec,
+		size_t bytes_transferred);
+
 void read() {
 	async_write(
-			socket_,
-			boost::asio::buffer(readBuffer_, BUFFER_SIZE),
+			sock,
+			boost::asio::buffer(readBuffer, BUFFER_SIZE),
 			onReadComplete
 	);
 }
 void write() {
 	async_write(
-			socket_,
-			boost::asio::buffer(writeBuffer_, BUFFER_SIZE),
-			boost::bind(
-					&Connection::onWriteComplete,
-					shared_from_this(),
-					_1,
-					_2
-					)
+			sock,
+			boost::asio::buffer(writeBuffer, BUFFER_SIZE),
+			onWriteComplete
+	);
+}
+
+void onConnectComplete(
+	const boost::system::error_code& ec,
+	boost::asio::ip::tcp::resolver::iterator i) {
+	if (!ec) {
+		read();
+		write();
+	} else {
+	}
+}
+
+void onResolveComplete(
+	const boost::system::error_code& ec,
+	boost::asio::ip::tcp::resolver::iterator i) {
+	if (!ec) {
+		boost::asio::ip::tcp::resolver::iterator end;
+		boost::asio::async_connect(
+				sock,
+				i,
+				end,
+				onConnectComplete
+			);
+	} else {
+	}
+}
+
+void resolve(std::string host, std::string port) {
+	boost::asio::ip::tcp::resolver::query q(host, port);
+	resolver.async_resolve(
+			q,
+			onResolveComplete
 	);
 }
 
 void onReadComplete(const boost::system::error_code& ec,
 		size_t bytes_transferred) {
 	if (!ec) {
+		read();
 	} else {
 		std::cerr << ec << std::endl;
 	}
@@ -39,14 +79,24 @@ void onReadComplete(const boost::system::error_code& ec,
 void onWriteComplete(const boost::system::error_code& ec,
 		size_t bytes_transferred) {
 	if (!ec) {
+		write();
 	} else {
 		std::cerr << ec << std::endl;
 	}
 }
 
 int main(int argc, char* argv[]) {
-	boost::asio::io_service ios;
-
+	if(argc != 3) {
+		std::cerr << "Usage:\n"
+			<< argv[0] << "<host> <port>"
+			<< std::endl; 
+			return EXIT_FAILURE;
+	}
+	for(std::size_t i = 0; i != BUFFER_SIZE; ++i) {
+		readBuffer[i] = i;
+		writeBuffer[i]	= i;
+	}
+	resolve(argv[1], argv[2]);
 	ios.run();
 	return EXIT_SUCCESS;
 }
