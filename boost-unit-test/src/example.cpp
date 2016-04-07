09d0812f17b805527735c5bca1acd65476942b42
xtwxy <xtwxy@hotmail.com>
2016-04-07 08:35:59 +0800

    add suit

--- boost-unit-test/src/example.cpp
+++ boost-unit-test/src/example.cpp
@@ -1,6 +1,11 @@
 #include <boost/test/unit_test.hpp>
 
+BOOST_AUTO_TEST_SUITE(test_suite_name);
+
 BOOST_AUTO_TEST_CASE( example_test ) {
 	BOOST_CHECK( true );
 	BOOST_CHECK_EQUAL( true, false );
 }
+
+
+BOOST_AUTO_TEST_SUITE_END();
--- boost-unit-test/src/main.cpp
+++ boost-unit-test/src/main.cpp
@@ -1,3 +1,4 @@
 #include <cstddef>
+
 #define BOOST_TEST_MODULE boost_unit_test
 #include <boost/test/included/unit_test.hpp>
