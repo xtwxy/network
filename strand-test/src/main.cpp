643a0802fcdf0d9d4c4db6dabfebeaaea3f93de0
Wangxy <xtwxy@hotmail.com>
2016-04-18 14:09:25 +0800

    add command arguments.

--- strand-test/src/main.cpp
+++ strand-test/src/main.cpp
@@ -15,27 +15,33 @@ io_service::strand _strand(_ios);
 void handler(int i, boost::function<void (int)> f) {
   std::cout << "handler(" << i << ") called." << std::endl;
   _ios.post(
-      _strand.wrap(
+      //_strand.wrap(
       boost::bind(f, i)
-      )
+      //)
       );
 }
 void complete(int i) {
   std::cout << "complete(" << i << ") called." << std::endl;
 }
 
-int main(int, char* [])
+int main(int argc, char* argv[])
 {
+  if(argc < 2) {
+    printf("Usage:\n%s <count>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  
+  const int count = atoi(argv[1]);
 
-  for(int i = 0; i < THREAD_COUNT*2048; ++i) {
+  for(int i = 0; i < count; ++i) {
     _ios.post(boost::bind(handler, i,
-                         _strand.wrap(
+                         //_strand.wrap(
                              complete
-                             )
+                             //)
                          )
              );
   }
   _ios.run();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
