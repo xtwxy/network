fe6ff5e4818cd8478c1e766815baf5d6b0ba043d
Wangxy <xtwxy@hotmail.com>
2016-04-18 11:47:24 +0800

    test strands.

---
+++ strand-test/docs/topics_on_serializing_requests.md
@@ -0,0 +1,3 @@
+1.the order of sequential is not guarranteed if multiple thread is used, even if boost::strand is used.
+2.the order of sequential is guarrenteed if single thread is used.
+
--- strand-test/src/main.cpp
+++ strand-test/src/main.cpp
@@ -3,24 +3,35 @@
 #include <boost/asio.hpp>
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+
+const int THREAD_COUNT = 8;
 
 using namespace boost::asio;
 
+io_service _ios;
+io_service::strand _strand(_ios);
+
 void handler(int i, boost::function<void (int)> f) {
   std::cout << "handler(" << i << ") called." << std::endl;
-  f(i);
+  _ios.post(
+      _strand.wrap(
+      boost::bind(f, i)
+      )
+      );
 }
 void complete(int i) {
   std::cout << "complete(" << i << ") called." << std::endl;
 }
-io_service _ios;
-io_service::strand _strand(_ios);
 
 int main(int, char* [])
 {
-  for(int i = 0; i < 10; ++i) {
+
+  for(int i = 0; i < THREAD_COUNT*2048; ++i) {
     _ios.post(boost::bind(handler, i,
-                         _strand.wrap(complete)
+                         _strand.wrap(
+                             complete
+                             )
                          )
              );
   }
