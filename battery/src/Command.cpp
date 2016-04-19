6a524f4ba9d11eb38db60d255d2a5cb774116b80
xtwxy <xtwxy@hotmail.com>
2016-04-19 22:56:57 +0800

    add encoding.

--- battery/src/Command.cpp
+++ battery/src/Command.cpp
@@ -28,7 +28,28 @@ Command::~Command() {
 }
 
 void Command::execute() {
-	encodeHeader();
+  Header* hdr = reinterpret_cast<Header*>(writeBuffer_);
+  hdr->Len = sizeof(Header);
+
+  char *data = (writeBuffer_ + sizeof(Header));
+  if(encoder_(data, 
+           BUFFER_SIZE, 
+           hdr->Addr, 
+           hdr->PackNo, 
+           hdr->Cmd, 
+           hdr->Len)) {
+    // encode success, add tail section (checksum, EOI),
+    Tail* tail = reinterpret_cast<Tail*>(writeBuffer_ + hdr->Len.value());
+    tail->Chksum = checksum(writeBuffer_, hdr->Len.value());
+    // and deliver package!
+    
+  } else {
+    // encode failed, notify the requester.
+    handler_(boost::system::errc::make_error_code(boost::system::errc::bad_message));
+  }
 }
 
+uint16_t Command::checksum(char* buff, std::size_t len) {
+  return 0;
+}
 } /* namespace battery */
